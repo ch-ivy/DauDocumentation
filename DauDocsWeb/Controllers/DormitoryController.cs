07370ccfba98baa682d0047e9fc259c4a01e8c98
Musa Jahun <msjahun@live.com>
2018-10-31 18:43:40 +0200

    initial commit

---
+++ DauDocsWeb/Controllers/DormitoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DauDocsWeb.Controllers
+{
+    public class DormitoryController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
